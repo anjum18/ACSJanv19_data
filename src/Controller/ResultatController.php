09eafe64aacefad88a41bb428b677bf230ed2c54
Neareeves <alethesj@gmail.com>
2019-02-01 14:43:19 +0100

    recherche ok

--- src/Controller/ResultatController.php
+++ src/Controller/ResultatController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Recherche;
+use App\Form\RechercheType;
+use App\Entity\Prenom;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ResultatController extends AbstractController
 {
@@ -12,8 +17,30 @@ class ResultatController extends AbstractController
      */
     public function index()
     {
+    	$em = $this->getDoctrine()->getManager();
+    	$prenom = $em->getRepository(Prenom::class)->findOneBy(['nom'=>'AADEL']);
+    	$name = $em->getRepository(Prenom::class)->findByNom(['AARON','CAMILLE']);
+    	$maxByYear = $em->getRepository(Prenom::class)->findMaxByYear('1905');
+
+    	$minByYear = $em->getRepository(Prenom::class)->findMinByYear('1905');
+    	$form = $this->createForm(RechercheType::class);
+        $form->handleRequest($request);
+        $minByYear=0;
+        if ($form->isSubmitted() && $form->isValid()) {
+ 
+            $contactFormData = $form->getData();
+ 
+            dump($contactFormData);
+        echo $contactFormData['year'];
+            // do something interesting here
+        $minByYear = $em->getRepository(Prenom::class)->findMinByYear($contactFormData['year']+1900);
+        }
         return $this->render('resultat/index.html.twig', [
             'controller_name' => 'ResultatController',
+            'page'=>'Prénoms d\'avant',
+    		'nom' => $name,
+    		'topNom'=>$maxByYear,
+    		'bottomNom' => $minByYear,
         ]);
     }
 }
